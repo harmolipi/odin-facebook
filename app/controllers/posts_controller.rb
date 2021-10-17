b253c0dc29441818cbc1fb049e9b67ae9b2d02dd
harmolipi <nikob381@gmail.com>
2021-10-17 18:10:21 -0400

    Created post routes and #index, #new, and #create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,25 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.includes(:user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
