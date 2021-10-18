2b4aa348f9d1194193958489489e85c80f191fe5
harmolipi <nikob381@gmail.com>
2021-10-18 15:11:33 -0400

    Added post spec tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'creating a post' do
+    before do
+      @user = User.new(name: Faker::Name.name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+    end
+
+    it 'requires text in the body' do
+      post = @user.posts.new
+      expect(post.valid?).to be false
+    end
+
+    it 'requires a user' do
+      post = Post.new(body: Faker::Lorem.paragraph)
+      expect(post.valid?).to be false
+    end
+
+    it 'creates successfully with user and text in the body' do
+      post = @user.posts.new(body: 'Hello world')
+      expect(post.valid?).to be true
+    end
+  end
 end
