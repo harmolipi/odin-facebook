83ab0bd9302f1954dfbd3c70257412c0c4ebe470
harmolipi <nikob381@gmail.com>
2021-10-18 15:11:28 -0400

    Added user spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Creating a user' do
+    it 'must have a name' do
+      expect { User.create!(email: Faker::Internet.email, password: 'password', password_confirmation: 'password') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'must have an email address' do
+      expect { User.create!(name: Faker::Name.name, password: 'password', password_confirmation: 'password') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'must have a password' do
+      expect { User.create!(name: Faker::Name.name, email: Faker::Internet.email) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'must have a unique email' do
+      test_email = Faker::Internet.email
+      User.create!(name: Faker::Name.name, email: test_email, password: 'password', password_confirmation: 'password')
+      expect { User.create!(name: Faker::Name.name, email: test_email, password: 'password', password_confirmation: 'password') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'successfully creates with all necessary attributes' do
+      expect { User.create!(name: Faker::Name.name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password') }.to_not raise_error      
+    end
+  end
 end
