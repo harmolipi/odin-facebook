597287428fe2fd6fc39065b1ad83184a8811d172
harmolipi <nikob381@gmail.com>
2021-10-18 15:22:43 -0400

    Created Friendship model specs

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+    @friend = User.create(name: Faker::Name.name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+  end
+
+  context 'friendship already exists' do
+    it 'fails to create friendship' do
+      @user.friendships.create(friend: @friend)
+      friendship = @user.friendships.new(friend: @friend)
+      expect(friendship).to_not be_valid
+    end
+  end
+
+  context 'friendship does not exist' do
+    context 'user and friend are the same' do
+      it 'fails to create friendship' do
+        friendship = @user.friendships.new(friend: @user)
+        expect(friendship).to_not be_valid
+      end
+    end
+
+    context 'user and friend are different' do
+      it 'creates friendship' do
+        friendship = @user.friendships.new(friend: @friend)
+        expect(friendship).to be_valid
+      end
+    end
+  end
 end
