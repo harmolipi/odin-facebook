5efe3d24a9ef0466a422fa6c1903e94764c4a074
harmolipi <nikob381@gmail.com>
2021-10-18 14:04:16 -0400

    Added name validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
   validates_associated :friendships
   
   has_many :posts, dependent: :destroy
