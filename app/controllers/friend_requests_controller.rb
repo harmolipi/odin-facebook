fac949e06196edf3930f0ed65e16fdd9be4e5400
harmolipi <nikob381@gmail.com>
2021-10-19 11:37:23 -0400

    Develop FriendRequestsController#index and #create

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,23 +1,25 @@
 class FriendRequestsController < ApplicationController
-  def index
+  before_action :authenticate_user!
 
+  def index
+    @friend_requests = current_user.received_friend_requests
   end
   
   # POST /friend_requests
   def create
-    @friend_request = current_user.friend_requests.new(friend_request_params)
+    @friend_request = current_user.sent_friend_requests.create(friend_request_params)
     if @friend_request.save
       flash[:notice] = 'Friend request sent!'
       redirect_to friend_requests_path
     else
       flash[:alert] = 'Friend request failed!'
-      redirect_to users_path
+      redirect_to user_path(friend_request_params[:requestee_id])
     end
   end
 
   private
 
   def friend_request_params
-    params.require(:friend_request).permit(:friend)
+    params.require(:friend_request).permit(:requester_id, :requestee_id)
   end
 end
