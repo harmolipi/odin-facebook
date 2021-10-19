15620275b1db177fffecc488748dddd76caaaee4
harmolipi <nikob381@gmail.com>
2021-10-19 11:51:11 -0400

    Added Friendships#index spec

--- spec/requests/friendships_spec.rb
+++ spec/requests/friendships_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Friendships", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user1) { User.create(name: 'rspec', email: 'rspec@test.com', password: 'password', password_confirmation: 'password') }
+  
+  describe 'GET /index' do
+    it 'returns http success' do
+      sign_in user1
+      get friendships_path
+      expect(response).to be_successful
+    end
   end
 end
