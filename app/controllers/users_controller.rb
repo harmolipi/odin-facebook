133679ad70e5178d2c8543198d54f0eea16f4a35
harmolipi <nikob381@gmail.com>
2021-10-21 15:00:42 -0400

    Added basic user profile biography functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,25 @@ class UsersController < ApplicationController
     @posts = @user.posts
     @post = Post.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User information updated"
+      redirect_to @user
+    else
+      flash[:error] = "Invalid user information"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:biography)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root 'posts#index'
   resources :posts, only: [:index, :new, :create, :show, :destroy]
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :friend_requests, only: [:index, :create, :destroy]
   resources :friendships, only: [:index, :create, :destroy]
   resources :likes, only: [:create, :destroy]
