d2cae05071e1199f3dcf6aa230ae4d0ba0261c38
harmolipi <nikob381@gmail.com>
2021-10-21 10:30:34 -0400

    Created User index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts, only: [:index, :new, :create, :show, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:index, :show]
   resources :friend_requests, only: [:index, :create, :destroy]
   resources :friendships, only: [:index, :create, :destroy]
   resources :likes, only: [:create, :destroy]
