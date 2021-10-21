80ff6cf4a1cfe74e0272e3c7317bf880577f7bec
harmolipi <nikob381@gmail.com>
2021-10-21 14:42:50 -0400

    Added biography attribut to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  biography              :text
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  name                   :string           not null
