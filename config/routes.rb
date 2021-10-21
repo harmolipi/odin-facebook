53bf2d9712bafd8bfb358166b13e2080965a0d35
harmolipi <nikob381@gmail.com>
2021-10-21 11:30:16 -0400

    Added name to User registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'posts#index'
   resources :posts, only: [:index, :new, :create, :show, :destroy]
   resources :users, only: [:index, :show]
